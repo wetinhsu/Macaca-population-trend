3ea0d19b900c6f766df8b995467e58bc1536298c
Wetin Hsu <wetinhsu@gmail.com>
2019-11-04 07:59:38 +0800

    v2.11

--- macaca2019.R
+++ macaca2019.R
@@ -64,3 +64,60 @@ Sys.time()
 
 
 write_xlsx(point.dat,"Macaca 2019/data/0_林管處_raw data/0_初檢核/point_Forest.xlsx")
+
+
+
+
+
+#==============================
+setwd("C:/Users/wetin/Desktop/R/Macaca-population-trend")
+
+
+point.list <- 
+  read_xlsx("Macaca 2019/data/0_林管處_raw data/0_初檢核/bind.xlsx",
+            sheet = "Sheet1",  range = "C1:W3132")  %>% 
+  setDT %>% 
+  .[,list(樣區編號, 旅次, 樣點編號,
+             TWD97_X = `X座標(TWD97)`,
+             TWD97_Y = `Y座標(TWD97)`)]%>%
+  .[!duplicated(TWD97_X,TWD97_Y)]  %>%
+  setDF
+
+point <- point.list
+names(point)
+
+coordinates(point) <- ~TWD97_X + TWD97_Y
+proj4string(point) <- CRS("+init=epsg:3826")
+
+
+## Set up container for results
+n <- length(point)
+nearest.Type <- character(n)
+nearest.dist <- numeric(n)
+
+
+
+Sys.time()
+
+for (i in seq_along(nearest.Type)) {
+  print(i)
+  Distance <-gDistance(point[i,], forest4th, byid=TRUE)
+  nearest.Type[i] <- forest4th@data$TypeName[which.min(Distance)] %>% as.character
+  nearest.dist[i] <- min(Distance)
+  
+}
+
+Sys.time()
+
+point.dat <- 
+  data.frame(Site_N = point@data$`樣區編號`,
+             Point = point@data$`樣點編號`,
+             trip = point@data$`旅次`,
+             TypeName = nearest.Type,
+             Distance = nearest.dist)
+
+
+Sys.time()
+
+
+write_xlsx(point.dat,"Macaca 2019/data/0_林管處_raw data/0_初檢核/survey_Forest.xlsx")
