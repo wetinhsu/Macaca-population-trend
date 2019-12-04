23e103c1fff93be38307ae43d0870af1f29878a3
wetinhsu <wetinhsu@gmail.com>
2019-12-04 17:54:36 +0800

    v4.5.2

--- script/analysis_GLMM_1113.R
+++ script/analysis_GLMM_1113.R
@@ -13,9 +13,11 @@ library(MuMIn)
 M.data <- 
   read_xlsx("data/clean/data_for_analysis_1519.xlsx") %>% 
   setDT %>% 
-  .[ Distance <20 , ] %>%
+  .[ Distance >20 | TypeName %in% c("待成林地", "裸露地", "陰影") , TypeName.1 := "Not forest"] %>%
+  
   .[, Year := as.numeric(Year)] %>% 
   .[, Year.re := Year - min(Year) + 1] %>%
+  
   .[County %in% list("宜蘭縣","基隆市","台北市","臺北市",
                      "新北市","台北縣","臺北縣",
                      "桃園縣","桃園市","新竹市",
@@ -33,15 +35,22 @@ M.data <-
   .[  Region %in% "East", Region.2 := "East"] %>%
   .[!(Region %in% "East"), Region.2 := "Wast"] %>%
   .[  Region %in% "North", Region.3 := "North"] %>%
-  .[ Region %in% "South", Region.3 := "South"]  %>%
-  .[ Region %in% "Center", Region.3 := "South"] %>%
-  .[ Region %in% "East", Region.3 := "South"] %>%
-  .[ , County := factor(County, labels = 1:23)] %>%
-  .[Year <2019,] %>%
+  .[ Region %in% c("South", "Center", "East"), Region.3 := "South"]  %>%
+  .[ , County := factor(County, labels = 1:25)] %>%
   .[is.na(Macaca_sur), Macaca_sur := 0]
+  
+  # 確認第1次調查，Survey為1 
+M.data %<>%  dcast.data.table(.,Year + Site_N ~ Survey, value.var = "Macaca_sur") %>%  
+                                                           #計算第1旅次及第2旅次調查的樣點數
+  .[`1` %in% 0,] %>%  #找出有第2旅次沒第1旅次的樣區
+  .[M.data, on = c("Year", "Site_N")] %>%  
+  .[!is.na(`1`), Survey := 1] %>%  #將第1次調查的旅次改回1
+  .[,`1` := NULL]%>%
+  .[,`2` := NULL]
 
 
-M.data$TypeName.1 %<>% factor(., labels = 1:4) # 竹林 = 1  針葉林= 2 混淆林=3 闊葉林=4
+
+M.data$TypeName.1 %<>% factor(., labels = c("Not forest",1:4))  # 竹林 = 1  針葉林= 2 混淆林=3 闊葉林=4
 M.data$TypeName.1 %<>% as.factor
 M.data$Altitude %<>% as.factor
 M.data$Year %<>% as.numeric
@@ -53,41 +62,25 @@ M.data$Region.3 %<>% as.factor
 
 M.data %>% .[, list(Macaca_sur, TypeName.1, Survey, Year, County, Region, Altitude)]  %>% plot
 #========================================================
-str(M.data)
 
-M.data %>%na.exclude %>%
-  .[, list(TypeName.1 = as.numeric(TypeName.1),
-           Macaca_sur = as.numeric(Macaca_sur),
-           Survey = as.numeric(Survey),
-           Year = as.numeric(Year),
-           County = as.numeric(County),
-           #Region.2 = as.numeric(Region.2), 
-           Altitude = as.numeric(Altitude))]  %>% setDF %>%
-  corPlot(.,stars=T,numbers=T, upper=F)
+M.data %>% 
+  
+  dcast(.,Year + Survey ~ TypeName.1, value.var = "Macaca_sur", fun = c(length,sum))
+  
+
+M.data %>% 
+  
+  dcast(.,Year + Survey ~ TypeName.1 + Altitude, value.var = "Macaca_sur", fun = c(sum))
 
 
 
+M.data %>% 
+  
+  dcast(., TypeName.1 ~ Altitude, value.var = "Macaca_sur", fun = c(sum))
 
-m1 <- glmer(Macaca_sur ~ TypeName.1  + Year.re +  Survey + (1|Site_N), 
-            family = binomial, data = df)
-Anova(m1)
-print(summary(m1),correlation=FALSE)
-anova(m1)
-
- +  Region.2
-
-
-summary(glht(m1, linfct = mcp(TypeName.1 = "Tukey")))
-summary(glht(m1, linfct = mcp(Year.re = "Tukey")))
-summary(glht(m1, linfct = mcp(Survey = "Tukey")))
-summary(glht(m1, linfct = mcp(Altitude = "Tukey")))
-summary(glht(m1, linfct = mcp(Region = "Tukey")))
-
-
-
-
-
-
+M.data %>% 
+  .[Macaca_sur %in% 1,] %>%
+  dcast(.,Year  ~ Macaca_dist, value.var = "Macaca_sur", fun =sum)
 
 #=====================
 
@@ -113,15 +106,21 @@ M.data %>% setDT %>% .[is.na(Macaca_sur),Macaca_sur:=0 ] %>%
 
 
 
+
 #==============================================
 df <- 
   M.data %>% 
-  .[!is.na(TypeName.1), ] 
+  .[Year < 2019,] %>%
+  .[!(TypeName.1 %in% "Not forest"), ] 
 
 df$TypeName.1<- relevel(df$TypeName.1, ref = "4")
 df$Region <- relevel(df$Region, ref = "East")
 
 
+
+
+  
+#full model==============================================
 allFit(glmer(Macaca_sur ~ TypeName.1  + Year.re + Altitude + Survey +  Region + (1|Site_N), 
              family = binomial, data = df))   #嘗試使用一系列優化程序重新擬合glmer模型
 
@@ -130,8 +129,18 @@ m1 <- glmer(Macaca_sur ~ Year.re + TypeName.1 + Altitude + Survey+  Region + (1|
             control = glmerControl(optimizer = "bobyqa"))
 
 summary(m1)
+
+#anova table==============================================
 Anova(m1)
 
+
+summary(glht(m1, linfct = mcp(TypeName.1 = "Tukey")))
+summary(glht(m1, linfct = mcp(Survey = "Tukey")))
+summary(glht(m1, linfct = mcp(Altitude = "Tukey")))
+summary(glht(m1, linfct = mcp(Region = "Tukey")))
+
+
+#AICc==============================================
 options(na.action = "na.fail")
 d1<- dredge(
   glmer(Macaca_sur ~ TypeName.1  + Year.re + Altitude + Survey+  Region + (1|Site_N), 
@@ -147,14 +156,13 @@ importance(d1)
 sw(model.avg(d1, subset = delta < 2))
 
 
+#Estimate==============================================
 bb<- df %>% setDT %>% .[is.na(Macaca_sur),Macaca_sur:=0 ] %>%
   .[!is.na(TypeName.1), .(Mean = mean(Macaca_sur, na.rm=T),
                           SD = sd(Macaca_sur, na.rm=T)/sqrt(length(Macaca_sur)),
                           n = .N), 
     by = list(TypeName.1,Survey, Altitude, Region)] %>%
   .[, N:= sum(n)]
-
-
 
 
 sum(bb$N*(bb$N-bb$n)*(bb$SD)^2/bb$n, na.rm=T)/(unique(bb$N)^2)
